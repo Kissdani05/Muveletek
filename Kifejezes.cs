cc1561118c02bd396d657ac705fb6e8324dcffbd
Kissdani05 <70641401+Kissdani05@users.noreply.github.com>
2023-09-14 11:05:01 +0200

    Add files via upload

---
+++ Kifejezes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Kifejezes
+    {
+
+        int szam1;
+        string muvelet;
+        int szam2;
+
+
+            public Kifejezes(string sor)
+            {
+                string[] darabok = sor.Split(" ");
+                this.szam1 = Convert.ToInt32(darabok[0]);
+                this.muvelet = darabok[1];
+                this.szam2 = Convert.ToInt32(darabok[2]);
+            }
+
+        public int Szam1 { get => szam1; }
+        public string Muvelet { get => muvelet;  }
+        public int Szam2 { get => szam2;  }
+    }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using ConsoleApp1;
+
+
+List<Kifejezes> kifejezesekListaja = new List<Kifejezes>();
+using (StreamReader sr = new StreamReader("kifejezesek.txt"))
+{
+    while (!sr.EndOfStream)
+    {
+        string sor = sr.ReadLine();
+        Kifejezes sor1 = new Kifejezes(sor);
+        kifejezesekListaja.Add(sor1);
+    }
+}
+
+Console.WriteLine(kifejezesekListaja.Count());
+int modulokSzama = 0;
+foreach (var kifejezes in kifejezesekListaja)
+{
+    if (kifejezes.Muvelet == "mod")
+    {
+        modulokSzama++;
+    }
+}
+
+modulokSzama = kifejezesekListaja.Count(x => x.Muvelet == "mod");
+
+Console.WriteLine(modulokSzama);
+int tizzel = 0;
+foreach (var kifejezes in kifejezesekListaja.Where(x => x.Szam1 % 10 == 0 & x.Szam2 % 10 == 0))
+{
+    tizzel++;
+    if (tizzel > 0)
+    {
+        Console.WriteLine("Van ilyen");
+        break;
+    }
+}
+
+if (kifejezesekListaja.Where(x => x.Szam1 % 10 == 0 & x.Szam2 % 10 == 0).Count() > 0)
+{
+        Console.WriteLine("Van ilyen");
+}
+
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "mod"));
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "/"));
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "div"));
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "-"));
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "*"));
+Console.WriteLine(kifejezesekListaja.Count(x => x.Muvelet == "+"));
